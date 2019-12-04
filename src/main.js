900bda8d986084ec7d5330d38f55e21e816fc1f4
aleks253545 <aleks253545@google.com>
2019-12-04 12:21:05 +0300

    async

--- src/main.js
+++ src/main.js
@@ -59,15 +59,32 @@ function renderSlider(source){
 
 }
 let param=3;
-$.getJSON('https://baconipsum.com/api/?callback=?', 
-	{ 'type':'meat-and-filler', 'start-with-lorem':'1', 'paras':`${param}` }, 
-	function(baconGoodness){
-        $.getJSON('https://picsum.photos/v2/list?',
-            {'page':'2','limit':`${param}`} ,
-            function(images){
-                baconGoodness.forEach((element,index)=> {
-                $('.posts').append($('<div class="post"></div>').append(`<img class=" post-img" src="${images[index].download_url}">`).append(`<span class="post-text">${baconGoodness[index]}</span>`));
-            });
-            });
-    });
+async function GetPosts(){
+    let responseText = await fetch(`https://baconipsum.com/api/?type=meat-and-filler&start-with-lorem=1&paras=${param}`);
+    let responseImg=await fetch(`https://picsum.photos/v2/list?page=2&limit=${param}`);
+    if (responseText.status == 200 && responseImg.status == 200) {
+        let jsonText = await responseText.json(); // (3)
+        let jsonImg = await responseImg.json(); // (3)
+        jsonText.forEach((element,index)=>{
+                $('.posts').append($('<div class="post"></div>').append(`<img class=" post-img" src="${jsonImg[index].download_url}">`).append(`<span class="post-text">${jsonText[index]}</span>`))
+        })
+        
+      }
+    else{
+      throw new Error(Error);
+    }
+}
+GetPosts();
+
+// $.getJSON('https://baconipsum.com/api/?callback=?', 
+// 	{ 'type':'meat-and-filler', 'start-with-lorem':'1', 'paras':`${param}` }, 
+// 	function(baconGoodness){
+//         $.getJSON('https://picsum.photos/v2/list?',
+//             {'page':'2','limit':`${param}`} ,
+//             function(images){
+//                 baconGoodness.forEach((element,index)=> {
+//                 $('.posts').append($('<div class="post"></div>').append(`<img class=" post-img" src="${images[index].download_url}">`).append(`<span class="post-text">${baconGoodness[index]}</span>`));
+//             });
+//             });
+//     });
 
